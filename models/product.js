f64e546f0d88e9b33f58b1fb55adc276f139bebe
Stanley Huang <infamous_godhand@yahoo.com>
2018-11-07 08:05:46 +0000

    dynamic passing parameter

--- models/product.js
+++ models/product.js
@@ -11,7 +11,8 @@ const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
-    } else {
+    }
+    else {
       cb(JSON.parse(fileContent));
     }
   });
@@ -26,6 +27,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
@@ -36,5 +38,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    })
   }
 };
